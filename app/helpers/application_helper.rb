7d11d172dfe40e874377b2755ab073fb36f95b38
Peter Spier <spierp@gmail.com>
2012-12-15 13:54:30 -0800

    layout changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def full_title(page_title)
+    base_title = 'HDCCo'
+    if page_title.empty?
+      base_title
+    else  
+      "#{base_title} | #{page_title}"
+    end
+  end
+  
 end
